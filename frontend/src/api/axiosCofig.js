0f71256f8a4961c0d12a54dc9db7908d286aecc2
1012abhi <130040653+1012abhi@users.noreply.github.com>
2025-01-07 20:06:03 +0530

    update axios configuration to use environment variable for base URL and add debug log

--- frontend/src/api/axiosCofig.js
+++ frontend/src/api/axiosCofig.js
@@ -1,10 +1,11 @@
 import axios from "axios"
 const token = localStorage.getItem('token')
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:8000/api',
+    baseURL: `${process.env.REACT_APP_BASE_URL}/api`,
     headers: {
         Authorization: `Bearer ${token}`
     }
 })
 console.log(token)
+console.log('Base URL:', process.env.REACT_APP_BASE_URL); // Add this for debugging
 export default axiosInstance

20af506aa7b779612acefd0883e9829264f6c6d5
1012abhi <130040653+1012abhi@users.noreply.github.com>
2025-01-14 11:03:42 +0530

    push for deployee

--- backend/app.js
+++ backend/app.js
@@ -16,6 +16,9 @@ app.use(cors({
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
 
 app.use('/api/auth',authRouter)
 app.use('/api/user',userRouter)

34390b22ab1e77f8a591ad22ac4b1ca1849ac963
1012abhi <130040653+1012abhi@users.noreply.github.com>
2025-01-18 14:52:16 +0530

    remove debug console logs from axios configuration

--- frontend/src/api/axiosCofig.js
+++ frontend/src/api/axiosCofig.js
@@ -6,6 +6,6 @@ const axiosInstance = axios.create({
         Authorization: `Bearer ${token}`
     }
 })
-console.log(token)
-console.log('Base URL:', process.env.REACT_APP_BASE_URL); // Add this for debugging
+// console.log(token)
+// console.log('Base URL:', process.env.REACT_APP_BASE_URL); // Add this for debugging
 export default axiosInstance
